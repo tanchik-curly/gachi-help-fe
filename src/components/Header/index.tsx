7094bb771f1a90cd3c4bde182c45127e53b93dfe
Elugormo <romanomess@gmail.com>
2022-11-09 20:49:44 +0200

    added redux stup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,46 @@
 import React from 'react';
+import { User } from 'interfaces';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Stack, Toolbar, Typography } from '@mui/material';
 
-const Header = () => {
+type Props = {
+  user: User;
+};
+
+const Header: React.FC<Props> = ({ user }: Props) => {
   const isAuth = true;
+
+  const formatUser = (user: User) => {
+    return `Logged in as ${user.firstName} ${user.lastName} a ${user.role}`;
+  };
 
   // todo: implement logging out
   const logoutHandler = () => {
     console.log('logged out');
   };
 
-  return isAuth ? (
+  return (
     <AppBar>
       <Toolbar>
-        <Typography>Gachi help</Typography>
-        {isAuth && (
-          <IconButton onClick={logoutHandler}>
-            <ExitToAppIcon />
-          </IconButton>
-        )}
+        <Stack
+          flexDirection="row"
+          alignItems="center"
+          sx={{ width: '100%' }}
+          justifyContent="space-between"
+        >
+          <Typography>Gachi help</Typography>
+          {isAuth && (
+            <Stack flexDirection="row" alignItems="center">
+              <Typography>{formatUser(user)}</Typography>
+              <IconButton onClick={logoutHandler}>
+                <ExitToAppIcon sx={{ color: '#fff' }} />
+              </IconButton>
+            </Stack>
+          )}
+        </Stack>
       </Toolbar>
     </AppBar>
-  ) : null;
+  );
 };
 
 export default Header;
